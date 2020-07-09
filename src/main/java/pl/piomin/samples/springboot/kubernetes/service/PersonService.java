f004702b040dc581d778ee8038fbccd25b7fc9f5
piomin <piotr.minkowski@gmail.com>
2020-07-09 14:12:56 +0200

    Removing listener

--- src/main/java/pl/piomin/samples/springboot/kubernetes/service/PersonService.java
+++ src/main/java/pl/piomin/samples/springboot/kubernetes/service/PersonService.java
@@ -4,15 +4,12 @@ import java.util.HashSet;
 import java.util.Set;
 
 import pl.piomin.samples.springboot.kubernetes.domain.Person;
-import pl.piomin.samples.springboot.kubernetes.listener.PersonAddEvent;
 import pl.piomin.samples.springboot.kubernetes.repository.PersonRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.transaction.support.TransactionSynchronizationAdapter;
-import org.springframework.transaction.support.TransactionSynchronizationManager;
 
 @Service
 public class PersonService {
@@ -28,7 +25,7 @@ public class PersonService {
 		for (Person p: persons) {
 			personSet.add(repository.save(p));
 		}
-		applicationEventPublisher.publishEvent(new PersonAddEvent());
+//		applicationEventPublisher.publishEvent(new PersonAddEvent());
 		return personSet;
 	}
 }
