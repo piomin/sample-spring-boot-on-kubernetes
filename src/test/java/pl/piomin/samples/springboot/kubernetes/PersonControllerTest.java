139100ef53faf23f9838c2f11462a0379379e10d
piomin <piotr.minkowski@gmail.com>
2023-01-07 10:31:50 +0100

    adding tests + renovate + circleci

--- src/test/java/pl/piomin/samples/springboot/kubernetes/PersonControllerTest.java
+++ src/test/java/pl/piomin/samples/springboot/kubernetes/PersonControllerTest.java
@@ -1,7 +1,6 @@
 package pl.piomin.samples.springboot.kubernetes;
 
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import pl.piomin.samples.springboot.kubernetes.domain.Gender;
 import pl.piomin.samples.springboot.kubernetes.domain.Person;
 
@@ -10,8 +9,13 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class PersonControllerTest {
+
+	private static String id;
 
 	@LocalServerPort
 	int port;
@@ -19,12 +23,30 @@ public class PersonControllerTest {
 	TestRestTemplate restTemplate;
 
 	@Test
-	void testAdd() {
+	@Order(1)
+	void add() {
 		Person person = new Person(null, "Test", "Test", 100, Gender.FEMALE);
 		Person personAdded = restTemplate.postForObject("/persons", person, Person.class);
-		Assertions.assertNotNull(personAdded);
-		Assertions.assertNotNull(personAdded.getId());
-		Assertions.assertEquals(person.getLastName(), personAdded.getLastName());
+		assertNotNull(personAdded);
+		assertNotNull(personAdded.getId());
+		assertEquals(person.getLastName(), personAdded.getLastName());
+		id = personAdded.getId();
+	}
+
+	@Test
+	@Order(2)
+	void findById() {
+		Person person = restTemplate.getForObject("/persons/{id}", Person.class, id);
+		assertNotNull(person);
+		assertNotNull(person.getId());
+		assertEquals(id, person.getId());
+	}
+
+	@Test
+	@Order(2)
+	void findAll() {
+		Person[] persons = restTemplate.getForObject("/persons", Person[].class);
+		assertEquals(6, persons.length);
 	}
 
 }
