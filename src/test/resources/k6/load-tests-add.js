d3da3571a5682de4eae26ca43dfa261c6515548e
piomin <piotr.minkowski@gmail.com>
2023-11-28 16:04:09 +0100

    update k6 tests

--- src/test/resources/k6/load-tests-add.js
+++ src/test/resources/k6/load-tests-add.js
@@ -3,6 +3,15 @@ import { check } from 'k6';
 import { randomIntBetween } from 'https://jslib.k6.io/k6-utils/1.2.0/index.js';
 import { randomString } from 'https://jslib.k6.io/k6-utils/1.2.0/index.js';
 
+export const options = {
+  duration: '60s',
+  vus: 5,
+  thresholds: {
+    http_req_failed: ['rate<0.25'],
+    http_req_duration: ['p(95)<1000'],
+  },
+};
+
 export default function () {
 
   const payload = JSON.stringify({
