7e25b23977ecd1d72a5797f2afa68d9bfadbecbd
piomin <piotr.minkowski@gmail.com>
2025-07-12 01:40:27 +0200

    remove lombok, change build image name

--- src/main/java/pl/piomin/samples/springboot/kubernetes/domain/Person.java
+++ src/main/java/pl/piomin/samples/springboot/kubernetes/domain/Person.java
@@ -1,25 +1,66 @@
 package pl.piomin.samples.springboot.kubernetes.domain;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(collection = "person")
-@Getter
-@Setter
-@AllArgsConstructor
-@NoArgsConstructor
 public class Person {
 
-	@Id
-	private String id;
-	private String firstName;
-	private String lastName;
-	private int age;
-	private Gender gender;
+    @Id
+    private String id;
+    private String firstName;
+    private String lastName;
+    private int age;
+    private Gender gender;
 
+    public Person() {
+    }
+
+    public Person(String id, String firstName, String lastName, int age, Gender gender) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.gender = gender;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public Gender getGender() {
+        return gender;
+    }
+
+    public void setGender(Gender gender) {
+        this.gender = gender;
+    }
 }
--- src/main/java/pl/piomin/samples/springboot/kubernetes/domain/PersonV2.java
+++ src/main/java/pl/piomin/samples/springboot/kubernetes/domain/PersonV2.java
@@ -1,23 +1,56 @@
 package pl.piomin.samples.springboot.kubernetes.domain;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(collection = "person")
-@Getter
-@Setter
-@AllArgsConstructor
-@NoArgsConstructor
 public class PersonV2 {
 
-	@Id
-	private String id;
-	private String name;
-	private int age;
-	private Gender gender;
+    @Id
+    private String id;
+    private String name;
+    private int age;
+    private Gender gender;
 
+    public PersonV2() {
+    }
+
+    public PersonV2(String id, String name, int age, Gender gender) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public Gender getGender() {
+        return gender;
+    }
+
+    public void setGender(Gender gender) {
+        this.gender = gender;
+    }
 }
